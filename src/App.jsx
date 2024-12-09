04d884219ca8ea594b3c168c0720cf448db0b0dd
Shrey Shukla <shreyshukla1010@gmail.com>
2024-12-09 17:10:35 +0530

    create-routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import CreateOrder from "./features/order/CreateOrder"
+import Order from "./features/order/Order"
+// import NotFound from "./ui/NotFound"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/menu",
+    element: <Menu/>
+  },
+  {
+    path: "/cart",
+    element : <Cart/>
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder/>
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order/>
+  }
+
+])
 
 
 function App(){
   return (
-    <div></div>
+   <RouterProvider router={router} />
   )
 }
 
