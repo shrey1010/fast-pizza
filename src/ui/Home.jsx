b6e6364ceee9b899b09003e08a9037b73f5dd87f
Shrey Shukla <shreyshukla1010@gmail.com>
2024-12-19 01:27:39 +0530

    feat: setting first redux state

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from 'react-redux';
 import CreateUser from '../features/user/CreateUser';
+import Button from './Button';
 function Home() {
+  const username = useSelector(state => state.user.username);
   return (
     <div className="my-10 sm:my-16 text-center px-4">
       <h1 className="text-xl font-semibold text-center text-stone-700 mb-8 md:text:3xl">
@@ -9,7 +12,7 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser/>
+      { username==="" ? <CreateUser/> :  <Button to="/menu" type="primary">Continue ordering, {username} </Button >}
     </div>
   );
 }
