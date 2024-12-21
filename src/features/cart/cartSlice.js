1b329770d1cbe9130a88fd2ba3168ee7f160ab17
Shrey Shukla <shreyshukla1010@gmail.com>
2024-12-22 03:36:45 +0530

    feat:adding menu to cart

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,56 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+ 
+
+ const initialState = {
+//   cart: [],
+    cart : [
+        {
+            pizzaId: 12,
+            name: 'Mediterranean',
+            quantity: 2,
+            unitPrice: 16,
+            totalPrice: 32, 
+        }
+    ],
+}   
+
+ const cartSlice =  createSlice({
+   name: "cart",
+   initialState,
+   reducers: {
+     addItem (state, action){
+         const item = state.cart.find((item) => item.pizzaId === action.payload.pizzaId)
+         if(item){
+             item.quantity++
+             item.totalPrice += item.unitPrice 
+         } else {
+             state.cart.push(action.payload)
+         }
+     },
+     deleteItem (state, action){
+         state.cart = state.cart.filter((item) => item.pizzaId !== action.payload)
+     },
+     increaseItemQuantity (state, action){
+         const item = state.cart.find((item) => item.pizzaId === action.payload)
+         if(item){
+             item.quantity++
+             item.totalPrice += item.unitPrice 
+         } 
+     },
+     decreaseItemQuntity   (state, action){
+         const item = state.cart.find((item) => item.pizzaId === action.payload)
+         if(item){
+             item.quantity--
+             item.totalPrice -= item.unitPrice 
+         } 
+     },
+     clearcart (state){
+         state.cart = [] 
+     },  
+   },
+ })
+
+
+  export const { addItem , deleteItem , increaseItemQuantity , decreaseItemQuantity , clearcart } = cartSlice.actions
+  export default cartSlice.reducer   
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,25 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
+
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1, 
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+     
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -12,7 +29,7 @@ function MenuItem({ pizza }) {
         <p className="font-semibold text-sm italic capitalize text-stone-500">{ingredients.join(', ')}</p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm font-semibold">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-red-500">Sold out</p>}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>  }
         </div>
       </div>
     </li>
